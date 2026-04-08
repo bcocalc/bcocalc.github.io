04de58165762e4b8672e9a37d1d81ed2273cab7e
Anthony <43102178+bcocalc@users.noreply.github.com>
2026-04-08 10:50:06 -0500

    Add files via upload

--- dev/service-worker.js
+++ dev/service-worker.js
@@ -1,13 +1,13 @@
-const CACHE_NAME = 'tapcalc-dev-cache-3.0.0-alpha110';
+const CACHE_NAME = 'tapcalc-dev-cache-3.0.0-alpha111';
 const ASSETS = [
   "./",
   "./index.html",
   "./measurement-card.html",
-  "./styles.css?v=3.0.0-alpha110",
-  "./measurement.js?v=3.0.0-alpha110",
+  "./styles.css?v=3.0.0-alpha111",
+  "./measurement.js?v=3.0.0-alpha111",
   "./script.js",
   "./manifest.json",
-  "./firebase-config.js?v=3.0.0-alpha110",
+  "./firebase-config.js?v=3.0.0-alpha111",
   "./team-logo.png"
 ];
 
