5da60e61fa48d5b9ea995bfdeb00db9e8588b113
Anthony <43102178+bcocalc@users.noreply.github.com>
2026-05-17 20:12:29 -0500

    Remove retired alpha217 glossary overlay

--- dev/tapcalc-alpha217-glossary-detach.js
+++
@@ -1,5 +0,0 @@
-/* TapCalc Dev 3.0.0-alpha219: alpha217 glossary detach retired.
-   Reference routing now lives in tapcalc-alpha219-reference-router.js. */
-(function(){
-  window.__tapcalcAlpha217GlossaryDetachReady = true;
-})();
