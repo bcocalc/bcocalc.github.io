1222a92740d9c1aa04cd23efc638dc053cdf465c
Anthony <43102178+bcocalc@users.noreply.github.com>
2026-05-17 20:12:21 -0500

    Remove retired alpha216 reference overlay

--- dev/tapcalc-alpha216-reference-fix.js
+++
@@ -1,5 +0,0 @@
-/* TapCalc Dev 3.0.0-alpha219: alpha216 glossary detach retired.
-   Reference routing now lives in tapcalc-alpha219-reference-router.js. */
-(function(){
-  window.__tapcalcAlpha216GlossaryDetachReady = true;
-})();
